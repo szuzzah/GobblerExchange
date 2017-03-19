1f3adeecf106021e3b461630c76bc2eaa81e5c9a
Megan Rigsbee <mrigsbee@vt.edu>
2017-03-18 22:14:11 -0400

    prepped config file for use with xampp

--- app/config.php
+++ app/config.php
@@ -2,13 +2,13 @@
 
 // path constants
 define('SYSTEM_PATH', dirname(__FILE__)); # location of 'app' folder - don't change
-define('BASE_URL','http://localhost/CRNShare');
+define('BASE_URL','http://localhost/GobblerExchange');
 
 // database constants
 define('DB_HOST','127.0.0.1');
 define('DB_USER','root');
 define('DB_PASS','');
-define('DB_DATABASE','crnshare');
+define('DB_DATABASE','gobbler_exchange');
 
 // admin credentials
 define('ADMIN_USERNAME', 'admin');
